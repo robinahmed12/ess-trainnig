261aaa7079c28fe4bc5c8d4da802ea7e42e96646
robinahmed12 <shawonahmed0294@gmail.com>
2025-08-26 11:37:07 +0600

    comapny use case of loop and array method

--- javascript_core/loop/loop_problem_solving.js
+++ javascript_core/loop/loop_problem_solving.js
@@ -1,3 +1,15 @@
+const products = [
+  { id: 1, name: "Laptop", price: 800 },
+  { id: 2, name: "Phone", price: 500 }
+];
+
+// Transform products to show price in USD
+
+
+const productList = products.map(p => `${p.name}-$${p.price}`)
+console.log(productList);
+
+
 const arr = [5, 10, 15, 20];
 // Calculate total sum using a for loop
 let sum = 0;
@@ -10,7 +22,7 @@ for (let i= 0; i < arr.length; i++) {
 
  const numbers = [3, 7, 2, 9, 5];
 // Find the largest number using a for loop
-const largest = numbers[0]
+let largest = numbers[0]
 for( let i = 1; i < numbers.length ; i++) {
     if(numbers[i] > largest) {
         largest = numbers[i]
@@ -18,3 +30,87 @@ for( let i = 1; i < numbers.length ; i++) {
 }
 
 console.log("Largest Number:", largest);
+
+
+const word = "programming";
+// Count number of vowels (a, e, i, o, u) using for loop
+let count = 0;
+const vowels = "aeiou"
+
+for (let i = 0; i < word.length; i++) {
+   if (vowels.includes(word[i])) {
+    count ++
+   }
+    
+}
+
+console.log(count);
+
+const user = { username: "john_doe", password: "1234" };
+// Check if "password" key exists using for...in
+let hasPassword = false
+for (const key in user) {
+  if (key === "password") {
+    hasPassword = true
+    break
+  }
+   
+}
+console.log(hasPassword ? "Password key exists" : "Password key does not exist");
+
+
+const students = {
+  alice: { age: 20, grade: "A" },
+  bob: { age: 22, grade: "B" }
+};
+
+// Print each student's name, age, and grade
+for (const student in students) {
+   console.log(student, students[student].age, students[student].grade );
+   
+}
+
+// shopping Cart Calculation
+const cart = [
+  { name: "Laptop", price: 50000, qty: 1 },
+  { name: "Headphones", price: 2000, qty: 2 },
+  { name: "Mouse", price: 1000, qty: 1 }
+];
+
+let total = 0
+for (const item of cart) {
+    total += item.price * item.qty
+}
+console.log("total bill", total );
+
+
+// Check User Permissions
+
+const userRoles = { admin: true, editor: false, viewer: true };
+for (const role in userRoles) {
+    if (userRoles[role]) {
+        console.log(`user has ${role} access`);
+        
+    }
+}
+
+
+
+
+
+
+
+
+// Performance Optimization
+
+// Industry Best Practices for Loops:
+
+// Use for...of for arrays, for...in for objects, .map(), .filter(), .reduce() for functional programming.
+
+// Break early when you find the result (break).
+
+// Avoid nested loops if possible (use maps/sets for O(1) lookups).
+
+// Keep loops small & optimize for large datasets (pagination, lazy loading).
+
+
